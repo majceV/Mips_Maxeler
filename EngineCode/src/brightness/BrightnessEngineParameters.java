8368568daf791ae2e66439d25da2afedd3e96c3d
makiVuk <maja.vu255@gmail.com>
2016-01-26 21:40:45 +0100

    Projekat

---
+++ EngineCode/src/brightness/BrightnessEngineParameters.java
@@ -0,0 +1,9 @@
+package brightness;
+
+import com.maxeler.maxcompiler.v2.build.EngineParameters;
+
+public class BrightnessEngineParameters extends EngineParameters {
+	public BrightnessEngineParameters(String[] args) {
+		super(args);
+	}
+}
